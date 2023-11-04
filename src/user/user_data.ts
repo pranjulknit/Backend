dd504c484555328e8ab0e7223fa09271218d1cd7
pranjulknit <chaurasiyapranjul@gmail.com>
2023-11-04 23:39:42 +0530

    first

--- src/user/user_data.ts
+++ src/user/user_data.ts
@@ -7,7 +7,7 @@ import * as path from "path";
 export class UserData{
     getAllUsers(callBack:(response:string| undefined)=>void):void{
         let userResponse:string;
-         let dir = "../src"
+
         fs.readFile(path.join(__dirname,"users_response.json"),"utf-8",(err,result)=>{
             //console.log(`result + ${__dirname}`);
             userResponse = result;
