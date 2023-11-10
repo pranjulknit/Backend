5dafb29aa2d2f3a49b5714243183ee5deb1f711c
pranjulknit <chaurasiyapranjul@gmail.com>
2023-11-10 13:20:05 +0530

    CRUD on mongodb

--- src/routing/user_routing.ts
+++ src/routing/user_routing.ts
@@ -3,7 +3,7 @@ import express, {response} from "express";
 import bcrypt from "bcrypt";
 import {validationResult} from "express-validator";
 import {getDatabase} from "../data/mongodb_client";
-import {Collection} from "mongodb";
+import {Collection, ObjectId} from "mongodb";
 
 const  userRouting = express.Router();
 
@@ -19,6 +19,70 @@ console.log("data ",data);
        "response":data
    })
 })
+
+
+userRouting.get("/getUsers",async(request:express.Request,response:express.Response)=>{
+    let db = getDatabase();
+    let userCollection = db.collection("users");
+
+    let data = await userCollection.find({}).toArray();
+
+    response.status(200).json({
+        "response":data,
+    })
+})
+
+
+userRouting.get("/getProfile/:id",async(request:express.Request,response:express.Response)=>{
+
+    let userId = request.params.userId;
+
+    let db = getDatabase();
+    let userCollection = db.collection("users");
+
+    let data = await userCollection.find({"_id":new ObjectId(userId)}).toArray();
+
+    response.status(200).json({
+        "response":data,
+    })
+})
+
+
+userRouting.put("/updateUser",async(request:express.Request,response:express.Response)=>{
+    let db = getDatabase();
+    let userCollection = db.collection("users");
+
+    const body = request.body;
+
+    const userObject = {
+        name:body.name,
+        following:body.following,
+        followers:body.followers,
+    }
+    let data = await userCollection.updateOne({_id:new  ObjectId(body.userId)},{$set:userObject});
+
+    response.status(200).json({
+        "response":data,
+    })
+})
+
+
+userRouting.delete("/deleteUser",async(request:express.Request,response:express.Response)=>{
+
+
+
+    let db = getDatabase();
+    let userCollection = db.collection("users");
+
+    const userId = request.body.userId;
+
+    let data = await userCollection.deleteOne({_id:new ObjectId(userId)});
+
+    response.status(200).json({
+        "response":data,
+    })
+})
+
 
 
 export default  userRouting;
